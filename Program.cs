499c5ec9bb2fe71c96a50615a996ca79f6fbd20c
Luck <lucas.mosquera13@gmail.com>
2020-01-21 22:02:24 -0300

    start

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace replace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ clsReplace.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace replace
+{
+    public class clsReplace
+    {
+        //Variables
+        public string String;
+
+        //Builder
+        public clsReplace(string pString)
+        {
+            String = pString;
+        }
+
+        //Convert string to array with spaces
+        public string[] toArray(string String)
+        {
+                string[] newArray = String.Split(' ');
+                return newArray;
+        }
+
+        //Convert commas to points
+        public string CommatoPoint(string String)
+        {
+            if (String.Contains(','))
+            {
+                String = String.Replace(',', '.');
+                return String;
+            }
+            else
+            {
+                Console.WriteLine("The string don't have commas to replace");
+                return String;
+            }
+        }
+
+        //Convert points to commas
+        public string PointtoComma(string String)
+        {
+            if (String.Contains('.'))
+            {
+                String = String.Replace('.', ',');
+                return String;
+            }
+            else
+            {
+                Console.WriteLine("The string don't have points to replace");
+                return String;
+            }
+        }
+
+    }
+}
